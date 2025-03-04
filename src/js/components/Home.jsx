e1eea860d9833651425036d1ad63933329d6b48d
Pedro Isaac Aguilar <vandeer.fler@gmail.com>
2025-03-04 23:48:14 +0000

    Todolist finalizada

--- src/js/components/Home.jsx
+++ src/js/components/Home.jsx
@@ -1,26 +1,46 @@
-import React from "react";
+import React, { useState } from "react";
+import "../../styles/index.css"
 
 //include images into your bundle
 import rigoImage from "../../img/rigo-baby.jpg";
 
 //create your first component
 const Home = () => {
+	const [value, setValue] = useState("");
+	const [tasks, setTasks] = useState([]);
 	return (
-		<div className="text-center">
-            
+		<div>
+			<div className="container">
+				<div className="input-group flex-nowrap">
+					<input type="text" className="form-control" placeholder="Write your task" aria-label="Task"
 
-			<h1 className="text-center mt-5">Hello Rigo!</h1>
-			<p>
-				<img src={rigoImage} />
-			</p>
-			<a href="#" className="btn btn-success">
-				If you see this green button... bootstrap is working...
-			</a>
-			<p>
-				Made by{" "}
-				<a href="http://www.4geeksacademy.com">4Geeks Academy</a>, with
-				love!
-			</p>
+						onChange={(event) => {
+							setValue(event.target.value);
+						}}
+						value={value}
+						onKeyDown={(event) => {
+							if (event.key == "Enter") {
+								console.log("Tarea agregada:", value);
+								setTasks([...tasks, value]);
+								setValue("");
+							}
+						}}
+					/>
+				</div>
+				<ul className="list-group">
+					{tasks.map((item, index) => (
+						<li className="list-group-item" key={index}>
+							{item}
+							<i onClick={() => {
+								const updateTodos = tasks.filter((_, currentIndex) => index != currentIndex);
+								setTasks(updateTodos)
+							}} className="fa-solid fa-trash icon"></i>
+						</li>))}
+				</ul>
+				<div className="counter">
+					{tasks.length} tasks pendings
+				</div>
+			</div>
 		</div>
 	);
 };
